b4d8a7ddb01d4f564eac97b506dc01087e893b80
husman2012 <37984589+husman2012@users.noreply.github.com>
2019-07-29 09:47:10 -0500

    Add files via upload

--- src/route_model.h
+++ src/route_model.h
@@ -1,5 +1,7 @@
-#pragma once
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
 
+#include <vector>
 #include <limits>
 #include <cmath>
 #include <unordered_map>
@@ -11,22 +13,25 @@ class RouteModel : public Model {
   public:
     class Node : public Model::Node {
       public:
-        // Add public Node variables and methods here.
-        
+        // Add public Node variables and methods here.      
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
       
       private:
         // Add private Node variables and methods here.
+        
         int index;
         RouteModel * parent_model = nullptr;
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
+  	auto &SNodes() {return m_Nodes;}
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
   private:
     // Add private RouteModel variables and methods here.
-
+	std::vector<Node> m_Nodes;
 };
+
+#endif
